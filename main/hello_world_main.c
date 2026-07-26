079546d3b7f04e8af158413ad19ddfcd4f67f7e0
yyj <y420614097@gmail.com>
2026-07-26 16:30:52 +0800

    esp32s3

---
+++ main/hello_world_main.c
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+void app_main(void)
+{
+    int count = 0;
+
+    while (1) {
+        printf("count = %d\n", count);
+        count++;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
