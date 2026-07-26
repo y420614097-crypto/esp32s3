485e20eb2b8c9777aa0035aadae690c82ad6e2fb
yyj <y420614097@gmail.com>
2026-07-26 19:44:17 +0800

    全部项目

---
+++ red_con/main/led_control.c
@@ -0,0 +1,19 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN 1
+
+void app_main(void)
+{
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while(1){
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+
+}
